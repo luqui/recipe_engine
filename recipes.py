d0bc1bb6b3e8bfe181993627fcfa603ca321faa5
Luke Palmer <luqui@google.com>
2015-08-26 18:29:40 -0700

    Super lazy fix.

--- recipes.py
+++ recipes.py
@@ -96,6 +96,7 @@ def run(package_deps, args):
 def roll(args):
   from recipe_engine import package
   repo_root, config_file = get_package_config(args)
+  repo_root = os.path.dirname(os.path.dirname(os.path.realpath(repo_root)))
   context = package.PackageContext.from_proto_file(repo_root, config_file)
   package_spec = package.PackageSpec.load_proto(config_file)
 
